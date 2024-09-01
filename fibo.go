0f469c83654dbc0c08994832278655690961ca6f
kaegeen <benabdessalemahmed9@gmail.com>
2024-09-01 20:29:06 +0200

    #

---
+++ fibo.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(Fibonacci(4))
+
+}
+
+func Fibonacci(index int) int {
+	if index < 0 {
+		return -1
+	}
+	if index == 0 {
+		return 0
+	}
+	if index == 1 {
+		return 1
+	}
+	return Fibonacci(index-1) + Fibonacci(index-2)
+}
